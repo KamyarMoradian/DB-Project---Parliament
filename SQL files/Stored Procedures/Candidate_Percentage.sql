92e8e787f79a0ea35c7f27ec7bdaa172c290a834
AmirMohammadKomijani <78591495+AmirMohammadKomijani@users.noreply.github.com>
2022-06-30 15:35:03 +0430

    Add files via upload

---
+++ SQL files/Stored Procedures/Candidate_Percentage.sql
@@ -0,0 +1,35 @@
+
+
+
+create procedure Candidate_Percentage @filter int
+as
+
+declare @num1 float
+set @num1 = 
+(select Count(t1.ID)
+from (
+select Candidate.ID, Candidate.F_Votes_no + Candidate.S_votes_no as total
+from Candidate
+where  Candidate.F_Votes_no + Candidate.S_votes_no < @filter 
+)as t1)
+
+--print @num1
+
+declare @num2 float
+set @num2 =
+(select Count(t2.ID)
+from(
+select Candidate.ID, Candidate.F_Votes_no + Candidate.S_votes_no as total
+from Candidate
+)as t2)
+
+--print @num2
+
+print (@num1 / @num2)*100
+go
+
+exec Candidate_Percentage @filter = 10
+exec Candidate_Percentage @filter = 7
+
+exec Candidate_Percentage @filter = 2000
+exec Candidate_Percentage @filter = 1000
